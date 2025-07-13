600219f15901669e6d8e04b5595780b5836be2e2
Harsh <harshsingh25665@gmail.com>
2025-07-13 20:43:40 +0530

    font update

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,13 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Nav from "./components/Nav";
+import{Roboto} from "next/font/google"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const roboto = Roboto({
+  subsets: ["latin"],
+  variable: "--font-roboto",
+  weight: ["100", "300", "400", "500", "700", "900"],
+})
 
 export const metadata = {
   title: "Moviehub",
@@ -22,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.variable} antialiased`}
       >
         <Nav/>
         {children}
